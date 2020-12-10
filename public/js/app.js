9691ffd1c1263b92ac30de1d400644b2bf52676c
Thomas <thom_car@yahoo.fr>
2020-12-10 22:06:28 +0100

    :white_check_mark: Add copy ip

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,29 @@
-const input = document.querySelector('button_ip');
-const btn  = document.querySelector('button')
+button_ip_popup = $("#button_ip_popup");
+button_ip_popup.hide();
+$(".button_ip_block").click(function () {
+
+    let range = document.createRange();
+    let selection = window.getSelection();
+    range.selectNode($("#button_ip")[0]);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    try {
+        var result = document.execCommand('copy');
+        button_ip_popup.show();
+        if(result){
+            setTimeout(function(){
+                button_ip_popup.hide();
+            }, 300);
+        }
+    }catch(err) {
+        alert(err);
+    }
+
+    selection = window.getSelection();
+    if(typeof selection.removeRange === 'function') {
+        selection.removeRange(range);
+    }else if(typeof  selection.removeAllRanges === 'function') {
+        selection.removeAllRanges();
+    }
+})
